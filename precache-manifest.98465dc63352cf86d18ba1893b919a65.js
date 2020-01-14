34427e0eca461fb4a7e57b9910a23471ff636d3b
Nandan <nandanprv@gmail.com>
2020-01-14 20:40:48 +0530

    Updates

---
+++ precache-manifest.98465dc63352cf86d18ba1893b919a65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d33f2cc4c107cccacdc11fc88088d7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3010b518ce7a13676b08",
+    "url": "/monsters-rolodex/static/css/main.d35ebe9e.chunk.css"
+  },
+  {
+    "revision": "733604e50e0426eb8c3d",
+    "url": "/monsters-rolodex/static/js/2.d471171a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d471171a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3010b518ce7a13676b08",
+    "url": "/monsters-rolodex/static/js/main.3309bce3.chunk.js"
+  },
+  {
+    "revision": "f89e514176ba9478383e",
+    "url": "/monsters-rolodex/static/js/runtime-main.038c5903.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
